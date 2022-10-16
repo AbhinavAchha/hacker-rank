bdbc932fa1f8aaa153fd20d570c77326a12ca5fa
Abhinav Achha <achhaabhinav@gmail.com>
2022-10-16 11:46:31 +0530

    ft(sql): add revising-the-select-query

---
+++ sql/revising-the-select-query.sql
@@ -0,0 +1,6 @@
+--  https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+SELECT * FROM CITY WHERE POPULATION > 100000 AND COUNTRYCODE = 'USA';
+
+
+-- https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
+SELECT NAME FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000;
